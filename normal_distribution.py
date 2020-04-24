5d27149bab8b106026fb86b759d910832c17104f
Robert Hillery <robert.hillery@gmail.com>
2020-04-23 20:24:15 -0400

    clt code

--- normal_distribution.py
+++ normal_distribution.py
@@ -1,7 +1,20 @@
 import math
 import numpy as np
+import distributions as ds
+import matplotlib.pyplot as plt
 
 # using the central limit theorem
+
+# start with a distribution, in this case a gamma distribution with the shape and sclae both equal to 2
+gamma_2_2 = ds.int_y_gamma_values_2_2
+
+sampled_data = [sum(np.random.choice(gamma_2_2, 70))/70 for _ in range(2500)]
+
+print(sampled_data[0], sampled_data[1], len(sampled_data))
+print(np.random.choice(gamma_2_2, 30))
+
+plt.hist(sampled_data)
+plt.show()
 
 # numpy normal distribution method
 mu, sigma = 0, 1
