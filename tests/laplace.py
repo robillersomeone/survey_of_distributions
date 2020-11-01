c6836d486df7375b3c4072c11a26f0618691334a
Robert Hillery <robert.hillery@gmail.com>
2020-11-01 13:33:35 -0500

    add initial laplace test

--- tests/laplace.py
+++ tests/laplace.py
@@ -5,6 +5,16 @@ import pytest
 import numpy as np
 import survey_of_distributions.distributions as ds
 
-# get support
+# x values for support in distributions
+x = np.arange(0, 20, .05)[1:]
+
+
+
+def test_laplace_distribution():
+    # might need the decimal for float types
+    scale = 1. 
+    loc = 0.
+    assert ds.laplace_distribution(x, 0, 1).all() == np.exp(-abs(x-loc)/scale)/(2.*scale).all()
+
 
 #log(uniform(0,1)/uniform(0,1)) ~ laplace(0,1)
